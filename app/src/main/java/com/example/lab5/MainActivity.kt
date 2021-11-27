8aaf1854b96bd1541d0f75859e5ca6f48eb5d2da
Tanya-00 <taki.fox8@gmail.com>
2021-11-27 15:41:49 +1000

    fixed

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -33,18 +33,13 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.movingcastle
+                R.id.home, R.id.movingcastle, R.id.spiritedaway, R.id.princessmononoke
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
--- app/src/main/java/com/example/lab5/home.kt
+++ app/src/main/java/com/example/lab5/home.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class movingcastle : Fragment() {
+class home : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
@@ -25,13 +26,13 @@ class movingcastle : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movingcastle, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            movingcastle().apply {
+            home().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
